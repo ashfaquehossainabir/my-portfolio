02969da4c763560231c5b0973e00afdfaa79a28e
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2024-09-17 21:43:23 +0600

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,61 +48,64 @@ const sitemap = [
 ];
 
 function Footer() {
-    return (
-        <footer className="section">
-            <div className="container">
-                <div className="lg:grid lg:grid-cols-2">
-                    <div className="mb-10">
-                        <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">Let&apos;s work together today!</h2>
-                        <ButtonPrimary
-                            href="mailto:ashfaquehossainXT@pm.me"
-                            label="Start Project"
-                            icon="chevron_right"
-                            classes="reveal-up"
-                        />
-                    </div>
+  const d = new Date();
+  let year = d.getFullYear();
 
-                    <div className="grid grid-cols-2 gap-4 lg:pl-20">
-                        <div>
-                            <p className="mt-2 mb-2 reveal-up">Sitemap</p>
+  return (
+      <footer className="section">
+          <div className="container">
+              <div className="lg:grid lg:grid-cols-2">
+                  <div className="mb-10">
+                      <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">Let&apos;s work together today!</h2>
+                      <ButtonPrimary
+                          href="mailto:ashfaquehossainXT@pm.me"
+                          label="Start Project"
+                          icon="chevron_right"
+                          classes="reveal-up"
+                      />
+                  </div>
 
-                            <ul>
-                                {
-                                    sitemap.map(({ label, href }, key) => (
-                                        <li key={ key }>
-                                            <a href={ href } className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up">{ label }</a>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
+                  <div className="grid grid-cols-2 gap-4 lg:pl-20">
+                      <div>
+                          <p className="mt-2 mb-2 reveal-up">Sitemap</p>
 
-                        <div>
-                            <p className="mt-2 mb-2 reveal-up">Socials</p>
+                          <ul>
+                              {
+                                  sitemap.map(({ label, href }, key) => (
+                                      <li key={ key }>
+                                          <a href={ href } className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up">{ label }</a>
+                                      </li>
+                                  ))
+                              }
+                          </ul>
+                      </div>
 
-                            <ul>
-                                {
-                                    socials.map(({ label, href }, key) => (
-                                        <li key={ key }>
-                                            <a href={ href } target="_blank" className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up">{ label }</a>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                      <div>
+                          <p className="mt-2 mb-2 reveal-up">Socials</p>
 
-                <div className="flex items-center justify-between pt-10 mb-8">
-                    <a href="/" className="logo reveal-up">
-                        <img src="/images/logo.svg" width={40} height={40} alt="Logo"/>
-                    </a>
+                          <ul>
+                              {
+                                  socials.map(({ label, href }, key) => (
+                                      <li key={ key }>
+                                          <a href={ href } target="_blank" className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up">{ label }</a>
+                                      </li>
+                                  ))
+                              }
+                          </ul>
+                      </div>
+                  </div>
+              </div>
 
-                    <p className="text-zinc-500 text-sm reveal-up">&copy; 2024 <span className="text-zinc-400">Ashfaque Hossain Abir</span></p>
-                </div>
-            </div>
-        </footer>
-    )
+              <div className="flex items-center justify-between pt-10 mb-8">
+                  <a href="/" className="logo reveal-up">
+                      <img src="/images/logo.svg" width={40} height={40} alt="Logo"/>
+                  </a>
+
+                  <p className="text-zinc-500 text-sm reveal-up">&copy; { year } <span className="text-zinc-400">Ashfaque Hossain Abir</span></p>
+              </div>
+          </div>
+      </footer>
+  )
 }
 
 export default Footer
